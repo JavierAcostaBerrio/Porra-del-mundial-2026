ed81e109cb279bbab8e17b56c0c0edaab04ccf8d
JavierAcostaBerrio <olijavier510@gmail.com>
2026-05-26 23:21:36 +0200

    Create estadisticas.js

---
+++ js/estadisticas.js
@@ -0,0 +1,72 @@
+// ===============================
+//   Porra Mundial 2026 - main.js
+// ===============================
+document.addEventListener("DOMContentLoaded", () => {
+    dibujarGraficoGoles();
+    dibujarGraficoCampeon();
+    dibujarGraficoMVP();
+    dibujarGraficoPichichi();
+    dibujarGraficoPichichiEspana();
+    dibujarGraficoGolesPichichiEspana();
+    dibujarGraficoTarjeta();
+    dibujarGraficoCapitanEspana();
+    dibujarGraficoEstrategiaEspana();
+    
+});
+
+console.log("Porra Mundial 2026 iniciada correctamente");
+
+
+async function cargarEstadisticas() {
+}
+// -------------------------------
+// Grafico goles
+// -------------------------------
+async function dibujarGraficoGoles() {
+}
+
+
+// -------------------------------
+// Grafico campeon
+// -------------------------------
+async function dibujarGraficoCampeon() {
+}
+
+// -------------------------------
+// Grafico MVP
+// -------------------------------
+async function dibujarGraficoMVP() {
+}
+// -------------------------------
+// Grafico Pichichi
+// -------------------------------
+async function dibujarGraficoPichichi() {
+}
+// -------------------------------
+// Grafico Pichichi España
+// -------------------------------
+async function dibujarGraficoPichichiEspana() {
+}
+
+// -------------------------------
+// Grafico Goles Pichichi España
+// -------------------------------
+async function dibujarGraficoGolesPichichiEspana() {
+}
+
+// -------------------------------
+// Grafico Primera Tarjeta España
+// -------------------------------
+async function dibujarGraficoTarjeta() {
+}
+
+// -------------------------------
+// Grafico Capitan España
+// -------------------------------
+async function dibujarGraficoCapitanEspana() {
+}
+// -------------------------------
+// Grafico Estrategia España
+// -------------------------------
+async function dibujarGraficoEstrategiaEspana() {
+}
